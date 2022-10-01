7af1aee9563673ce03d381f45724c75f17608271
bernarda.crnogorac <beacrnogorac@gmail.com>
2022-10-01 13:01:19 +0200

    tailwind vue setup

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1 class="text-orange-800">{{ msg }}</h1>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld.Vue",
+  data() {
+    return {
+      msg: "rulet",
+    };
+  },
+};
+</script>
